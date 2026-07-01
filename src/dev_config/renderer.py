382d929bb86a3aa83c90228557426b32b85fd66c
James Rudd <jamesdrudd@protonmail.com>
2026-07-01 11:46:47 -0600

    Final update

--- src/dev_config/renderer.py
+++ src/dev_config/renderer.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from jinja2 import Environment, FileSystemLoader
+
 
 def render_templates(config, template_dir, output_dir, templates):
     env = Environment(loader=FileSystemLoader(template_dir))
@@ -8,5 +11,7 @@ def render_templates(config, template_dir, output_dir, templates):
         rendered = template.render(**config)
 
         out_name = tname.replace(".j2", "")
-        with open(f"{output_dir}/{out_name}", "w") as f:
+        out_path = Path(output_dir) / out_name
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(out_path, "w") as f:
             f.write(rendered)
